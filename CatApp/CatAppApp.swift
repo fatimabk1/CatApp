322570103686f473ffb6be3835c211a2a516e58d
fatimabk1 <fatima.kahbi@gmail.com>
2024-05-25 19:43:28 -0400

    Initial Commit

---
+++ CatApp/CatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatAppApp.swift
+//  CatApp
+//
+//  Created by Fatima Kahbi on 5/25/24.
+//
+
+import SwiftUI
+
+@main
+struct CatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
